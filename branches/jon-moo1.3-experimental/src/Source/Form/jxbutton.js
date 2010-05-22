fd5f9afed04ab5a872995abf1587a40142c6dcf1
jonlb@comcast.net <jonlb@comcast.net@0cc02af8-7455-0410-a6ac-531894676941>
2010-05-22 20:48:11 +0000

    remove apostrophe from branch name

---
+++ branches/jon-moo1.3-experimental/src/Source/Form/jxbutton.js
@@ -0,0 +1,83 @@
+/*
+---
+
+name: Jx.Field.Button
+
+description: Represents a button input
+
+license: MIT-style license.
+
+requires:
+- Jx.Field
+- Jx.Button
+
+provides: [Jx.Field.Button]
+
+...
+ */
+/**
+ * Class: Jx.Field.Button
+ *
+ * Extends: <Jx.Field>
+ *
+ * This class represents a button.
+ *
+ * Example:
+ * (code)
+ * (end)
+ *
+ * License:
+ * Copyright (c) 2009, DM Solutions Group
+ *
+ * This file is licensed under an MIT style license
+ */
+Jx.Field.Button = new Class({
+
+    Extends: Jx.Field,
+
+    options: {
+        /**
+         * Option: buttonClass
+         * choose the actual Jx.Button subclass to create for this form
+         * field.  The default is to create a basic Jx.Button.  To create
+         * a different kind of button, pass the class to this option, for
+         * instance:
+         * (code)
+         * buttonClass: Jx.Button.Color
+         * (end)
+         */
+        buttonClass: Jx.Button,
+        
+        /**
+         * Option: buttonOptions
+         */
+        buttonOptions: {},
+        /**
+         * Option: template
+         * The template used to render this field
+         */
+        template: '<span class="jxInputContainer"><label class="jxInputLabel"></label><div class="jxInputButton"></div><span class="jxInputTag"></span></span>'
+    },
+    
+    button: null,
+    
+    /**
+     * Property: type
+     * The type of this field
+     */
+    type: 'Button',
+
+    processTemplate: function(template, classes, container) {
+        var h = this.parent(template, classes, container);
+        this.button = new this.options.buttonClass(this.options.buttonOptions);
+        var c = h.get('jxInputButton');
+        if (c) {
+            this.button.domObj.replaces(c);
+        }
+        return h;
+    },
+    
+    click: function() {
+        this.button.clicked();
+    }
+});
